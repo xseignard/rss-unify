387ab481169f7fbe5f8f46f5c090aa9042432cd7
xseignard <xavier.seignard@gmail.com>
2013-02-25 17:52:14 +0100

    corrected bug about setinterval call

--- src/server/core/cache.js
+++ src/server/core/cache.js
@@ -13,13 +13,14 @@ var Cache = function(topics, redis) {
 		// first caching
 		_cache();
 		// repeatedly call that to re-aggregate the feed
-		setInterval(Cache.cache, intervalInMillis);
+		setInterval(_cache, intervalInMillis);
 	};
 
 	/**
 	 * Cache feeds
 	 */
 	var _cache = function(callback) {
+		console.log('Updating cache...');
 		var storeIt = function(key, feed) {
 			// remove previous cached version (if existing) and store the new one on redis
 			// the feed is stored in two representations:
@@ -29,6 +30,7 @@ var Cache = function(topics, redis) {
 			redis.del(key + suffix, redis.print);
 			redis.set(key, JSON.stringify(feed), redis.print);
 			redis.set(key + suffix, feed.xml(), redis.print);
+			console.log('Cache for ' + key + ' updated');
 			if (callback) callback();
 		};
 		// cache aggregated feed for each topic
